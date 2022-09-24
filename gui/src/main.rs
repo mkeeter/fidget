6c22eb725f7a9c13f9ed2fd3c0d23105efde856d
Matt Keeter <matt.j.keeter@gmail.com>
2022-09-24 09:09:58 -0400

    GUI now renders something!

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,24 +2,30 @@
 
 use std::sync::{Arc, Mutex};
 
-use eframe::egui::{self, widgets::text_edit::TextEdit, FontId};
-use fidget::context::{Context, Node};
-use rhai::{Dynamic, Engine};
+use eframe::egui;
+use fidget::render::RenderConfig;
+
+mod highlight;
 
 fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
-        "fidget",
+        "Fidget",
         options,
         Box::new(|_cc| Box::new(MyApp::default())),
     );
 }
 
 struct MyApp {
-    engine: Engine,
+    first_run: bool,
+    label_height: Option<f32>,
+    textures: Vec<egui::TextureHandle>,
+
+    engine: rhai::Engine,
+    script_ctx: Arc<Mutex<ScriptContext>>,
+
     script: String,
     out: String,
-    label_height: Option<f32>,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,42 +42,52 @@ impl FidgetContext for rhai::NativeCallContext<'_> {
     where
         F: Fn(&mut fidget::context::Context) -> V,
     {
-        let ctx = self.tag().unwrap().clone_cast::<Arc<Mutex<Context>>>();
-        let mut lock = ctx.lock().unwrap();
-        f(&mut lock)
+        let ctx = self
+            .tag()
+            .unwrap()
+            .clone_cast::<Arc<Mutex<ScriptContext>>>();
+        let lock = &mut ctx.lock().unwrap().fidget_context;
+        f(lock)
+    }
+}
+
+struct ScriptContext {
+    fidget_context: fidget::context::Context,
+    shapes: Vec<fidget::context::Node>,
+}
+
+impl ScriptContext {
+    fn new() -> Self {
+        Self {
+            fidget_context: fidget::context::Context::new(),
+            shapes: vec![],
+        }
+    }
+    fn clear(&mut self) {
+        self.fidget_context.clear();
+        self.shapes.clear();
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/*
-fn circle(cx, cy, r) {
-    let c = |x, y| {
-        sqrt((x - cx) * (x - cx) +
-             (y - cy) * (y - cy)) - r
-    };
-    print(c);
-    c
-}
-
-let r = 1;
-let f = circle(0, 0, 2);
-r = 1;
-print(f.call(1.0, 0.0));
-
-*/
-
-fn var_x(ctx: rhai::NativeCallContext) -> Node {
+fn var_x(ctx: rhai::NativeCallContext) -> fidget::context::Node {
     ctx.with_fidget_context(|c| c.x())
 }
-fn var_y(ctx: rhai::NativeCallContext) -> Node {
+fn var_y(ctx: rhai::NativeCallContext) -> fidget::context::Node {
     ctx.with_fidget_context(|c| c.y())
+}
+
+fn draw(ctx: rhai::NativeCallContext, node: fidget::context::Node) {
+    let ctx = ctx.tag().unwrap().clone_cast::<Arc<Mutex<ScriptContext>>>();
+    ctx.lock().unwrap().shapes.push(node);
 }
 
 macro_rules! define_binary_fns {
     ($name:ident) => {
         mod $name {
             use super::*;
+            use fidget::context::Node;
             use rhai::NativeCallContext;
             pub fn node_node(ctx: NativeCallContext, a: Node, b: Node) -> Node {
                 ctx.with_fidget_context(|c| c.$name(a, b).unwrap())
@@ -108,6 +124,7 @@ macro_rules! define_unary_fns {
     ($name:ident) => {
         mod $name {
             use super::*;
+            use fidget::context::Node;
             use rhai::NativeCallContext;
             pub fn node(ctx: NativeCallContext, a: Node) -> Node {
                 ctx.with_fidget_context(|c| c.$name(a).unwrap())
@@ -142,10 +159,11 @@ macro_rules! register_unary_fns {
 
 impl Default for MyApp {
     fn default() -> Self {
-        let mut engine = Engine::new();
-        engine.register_type_with_name::<Node>("Node");
-        engine.register_fn("var_x", var_x);
-        engine.register_fn("var_y", var_y);
+        let mut engine = rhai::Engine::new();
+        engine.register_type_with_name::<fidget::context::Node>("Node");
+        engine.register_fn("__var_x", var_x);
+        engine.register_fn("__var_y", var_y);
+        engine.register_fn("__draw", draw);
 
         register_binary_fns!("+", add, engine);
         register_binary_fns!("-", sub, engine);
@@ -158,10 +176,21 @@ impl Default for MyApp {
 
         engine.set_fast_operators(false);
 
-        let fidget_ctx = Dynamic::from(Arc::new(Mutex::new(Context::new())));
-        engine.set_default_tag(fidget_ctx);
+        let script_ctx = Arc::new(Mutex::new(ScriptContext::new()));
+        engine.set_default_tag(rhai::Dynamic::from(script_ctx.clone()));
+
+        let ast = engine
+            .compile(include_str!("../scripts/core.rhai"))
+            .unwrap();
+        let module =
+            rhai::Module::eval_ast_as_new(rhai::Scope::new(), &ast, &engine)
+                .unwrap();
+        engine.register_global_module(rhai::Shared::new(module));
 
         Self {
+            first_run: true,
+            textures: vec![],
+            script_ctx,
             engine,
             script: "// hello, world".to_owned(),
             out: "".to_string(),
@@ -170,8 +199,30 @@ impl Default for MyApp {
     }
 }
 
+impl MyApp {
+    fn init(&mut self, ctx: &egui::Context) {
+        let mut theme = egui::Visuals::dark();
+
+        let f = |c: syntect::highlighting::Color| {
+            egui::Color32::from_rgb(c.r, c.g, c.b)
+        };
+        let sol = crate::highlight::get_theme().settings;
+
+        theme.extreme_bg_color = sol.background.map(f).unwrap();
+        theme.widgets.noninteractive.bg_fill = sol.gutter.map(f).unwrap();
+        theme.selection.bg_fill = sol.selection.map(f).unwrap();
+
+        ctx.set_visuals(theme);
+    }
+}
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if self.first_run {
+            self.init(ctx);
+            self.first_run = false;
+        }
+
         egui::SidePanel::left("root")
             .default_width(400.0)
             .show(ctx, |ui| {
@@ -182,19 +233,12 @@ impl eframe::App for MyApp {
                     style.spacing.item_spacing.y += PAD;
                     size.y -= h + 2.0 * PAD;
                 }
-                let r = ui.add_sized(
-                    size,
-                    TextEdit::multiline(&mut self.script)
-                        .font(FontId::monospace(16.0)),
-                );
+                let r =
+                    crate::highlight::code_view_ui(ui, &mut self.script, size);
                 if r.changed() || self.label_height.is_none() {
-                    let c = self
-                        .engine
-                        .default_tag()
-                        .clone_cast::<Arc<Mutex<Context>>>();
-                    c.lock().unwrap().clear();
-
+                    self.script_ctx.lock().unwrap().clear();
                     let v = self.engine.run(&self.script);
+
                     self.out = format!("{:?}", v);
                 }
 
@@ -203,6 +247,76 @@ impl eframe::App for MyApp {
                 if Some(new_height) != self.label_height {
                     self.label_height = Some(new_height);
                     ctx.request_repaint();
+                }
+            });
+
+        {
+            let script_ctx = self.script_ctx.lock().unwrap();
+            for (i, s) in script_ctx.shapes.iter().enumerate() {
+                let tape = script_ctx
+                    .fidget_context
+                    .get_tape(*s, fidget::asm::dynasm::REGISTER_LIMIT);
+                let image = fidget::render::render::<
+                    fidget::asm::dynasm::JitEvalFamily,
+                >(
+                    tape,
+                    &RenderConfig {
+                        image_size: 512,
+                        tile_size: 64,
+                        subtile_size: 8,
+                        threads: 8,
+                        interval_subdiv: 3,
+                    },
+                );
+                let pixels = image
+                    .into_iter()
+                    .map(|p| {
+                        let [r, g, b, a] = p.as_color();
+                        egui::Color32::from_rgba_unmultiplied(r, g, b, a)
+                    })
+                    .collect::<Vec<_>>();
+                let image = egui::ImageData::Color(egui::ColorImage {
+                    size: [512; 2],
+                    pixels,
+                });
+
+                match self.textures.get_mut(i) {
+                    Some(t) => t.set(image, egui::TextureFilter::Linear),
+                    None => {
+                        let texture = ctx.load_texture(
+                            "tex",
+                            image,
+                            egui::TextureFilter::Linear,
+                        );
+                        self.textures.push(texture);
+                    }
+                }
+            }
+        }
+
+        egui::CentralPanel::default()
+            .frame(egui::Frame::none().fill(egui::Color32::BLACK))
+            .show(ctx, |ui| {
+                let pos = ui.next_widget_position();
+                let size = ui.available_size();
+                let painter = ui.painter_at(egui::Rect {
+                    min: pos,
+                    max: pos + size,
+                });
+                for t in &self.textures {
+                    let mut mesh = egui::Mesh::with_texture(t.id());
+                    mesh.add_rect_with_uv(
+                        egui::Rect {
+                            min: pos,
+                            max: pos + size,
+                        },
+                        egui::Rect {
+                            min: egui::Pos2::new(0.0, 0.0),
+                            max: egui::Pos2::new(1.0, 1.0),
+                        },
+                        egui::Color32::WHITE,
+                    );
+                    painter.add(mesh);
                 }
             });
     }
---
+++ gui/syntax/README.md
@@ -0,0 +1,3 @@
+Based on [sublime-rhai](https://github.com/rhaiscript/sublime-rhai)
+
+Downloaded on 2022-09-23, commit `3d973e99bb58df253f1e4b3c56baa776e2718451`
