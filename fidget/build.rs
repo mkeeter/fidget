9b672e1f3b24d030e6de2423f1dc45fa01aeecb8
Matt Keeter <matt.j.keeter@gmail.com>
2023-03-08 13:14:52 -0500

    Add better feature checking

--- fidget/build.rs
+++ fidget/build.rs
@@ -18,6 +18,28 @@ fn next(axis: usize) -> usize {
 }
 
 fn main() {
+    // Check CPU feature support and error out if we don't have the appropriate
+    // features. This isn't a fool-proof – someone could build on a machine with
+    // AVX2 support, then try running those binaries elsewhere – but is a good
+    // first line of defense.
+    if std::env::var("CARGO_FEATURE_JIT").is_ok() {
+        #[cfg(target_arch = "x86_64")]
+        if !std::arch::is_x86_feature_detected!("avx2") {
+            eprintln!(
+                "`x86_64` build with `jit` enabled requires AVX2 instructions"
+            );
+            std::process::exit(1);
+        }
+
+        #[cfg(target_arch = "aarch64")]
+        if !std::arch::is_aarch64_feature_detected!("neon") {
+            eprintln!(
+                "`aarch64` build with `jit` enabled requires NEON instructions"
+            );
+            std::process::exit(1);
+        }
+    }
+
     if std::env::var("CARGO_FEATURE_MESH").is_ok() {
         build_mdc_table();
     }
