1f49d69f6d360cd48fe365562d691cda5f4502ba
Matt Keeter <matt.j.keeter@gmail.com>
2023-03-29 08:38:17 -0400

    Add more benchmarking

--- fidget/benches/render.rs
+++ fidget/benches/render.rs
@@ -1,34 +1,86 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{
+    black_box, criterion_group, criterion_main, BenchmarkId, Criterion,
+};
 
 const PROSPERO: &str = include_str!("../../models/prospero.vm");
 
 use fidget::{eval::Family, jit::Eval};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    let cfg = fidget::render::RenderConfig {
-        image_size: 1024,
-        tile_sizes: Eval::tile_sizes_2d().to_vec(),
-        threads: 8,
-
-        mat: nalgebra::Transform2::identity(),
-    };
-    // fooo
+pub fn prospero_size_sweep(c: &mut Criterion) {
     let (ctx, root) = fidget::Context::from_text(PROSPERO.as_bytes()).unwrap();
-    let tape = ctx.get_tape::<Eval>(root).unwrap();
+    let tape_jit = &ctx.get_tape::<fidget::jit::Eval>(root).unwrap();
+    let tape_vm = &ctx.get_tape::<fidget::vm::Eval>(root).unwrap();
 
-    c.bench_function("render 1024x1024", move |b| {
-        let tape_ref = &tape;
-        let cfg_ref = &cfg;
-        b.iter(|| {
-            let tape = tape_ref.clone();
-            black_box(fidget::render::render2d(
-                tape,
-                cfg_ref,
-                &fidget::render::BitRenderMode,
-            ))
-        })
-    });
+    let mut group =
+        c.benchmark_group("speed vs image size (prospero, 2d) (8 threads)");
+    for size in [256, 512, 768, 1024, 1280, 1546, 1792, 2048] {
+        let cfg = &fidget::render::RenderConfig {
+            image_size: size,
+            tile_sizes: Eval::tile_sizes_2d().to_vec(),
+            threads: 8,
+
+            mat: nalgebra::Transform2::identity(),
+        };
+        group.bench_function(BenchmarkId::new("jit", size), move |b| {
+            b.iter(|| {
+                let tape = tape_jit.clone();
+                black_box(fidget::render::render2d(
+                    tape,
+                    cfg,
+                    &fidget::render::BitRenderMode,
+                ))
+            })
+        });
+        group.bench_function(BenchmarkId::new("vm", size), move |b| {
+            b.iter(|| {
+                let tape = tape_vm.clone();
+                black_box(fidget::render::render2d(
+                    tape,
+                    cfg,
+                    &fidget::render::BitRenderMode,
+                ))
+            })
+        });
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn prospero_thread_sweep(c: &mut Criterion) {
+    let (ctx, root) = fidget::Context::from_text(PROSPERO.as_bytes()).unwrap();
+    let tape_jit = &ctx.get_tape::<fidget::jit::Eval>(root).unwrap();
+    let tape_vm = &ctx.get_tape::<fidget::vm::Eval>(root).unwrap();
+
+    let mut group =
+        c.benchmark_group("speed vs threads (prospero, 2d) (1024 x 1024)");
+    for threads in [1, 2, 4, 8, 16] {
+        let cfg = &fidget::render::RenderConfig {
+            image_size: 1024,
+            tile_sizes: Eval::tile_sizes_2d().to_vec(),
+            threads,
+
+            mat: nalgebra::Transform2::identity(),
+        };
+        group.bench_function(BenchmarkId::new("jit", threads), move |b| {
+            b.iter(|| {
+                let tape = tape_jit.clone();
+                black_box(fidget::render::render2d(
+                    tape,
+                    cfg,
+                    &fidget::render::BitRenderMode,
+                ))
+            })
+        });
+        group.bench_function(BenchmarkId::new("vm", threads), move |b| {
+            b.iter(|| {
+                let tape = tape_vm.clone();
+                black_box(fidget::render::render2d(
+                    tape,
+                    cfg,
+                    &fidget::render::BitRenderMode,
+                ))
+            })
+        });
+    }
+}
+
+criterion_group!(benches, prospero_size_sweep, prospero_thread_sweep);
 criterion_main!(benches);
