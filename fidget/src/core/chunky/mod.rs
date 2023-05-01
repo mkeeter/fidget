bafb1794f7a9eca19eb0ef00b2f02852d3b0e4f7
Matt Keeter <matt.j.keeter@gmail.com>
2023-04-30 20:09:07 -0400

    awww yeah inlining

--- fidget/src/core/chunky/mod.rs
+++ fidget/src/core/chunky/mod.rs
@@ -36,9 +36,69 @@ impl<'a> Compiler<'a> {
         }
     }
 
+    /// Compute which nodes should be inlined
+    fn pick_inline(
+        ctx: &Context,
+        root: Node,
+        max_weight: usize,
+    ) -> BTreeSet<Node> {
+        enum Action {
+            Down,
+            Up,
+        }
+
+        // Recursively assign weights to nodes in the graph, using a loop with
+        // manual recursion to avoid blowing the stack.
+        //
+        // A rank is `sum(rank(c) for c in children) + 1`, i.e. the total number
+        // of operations required for a node.
+        let mut todo = vec![(Action::Down, root)];
+        let mut weights = BTreeMap::new();
+        let mut inline = BTreeSet::new();
+        while let Some((a, node)) = todo.pop() {
+            if weights.contains_key(&node) {
+                continue;
+            }
+            match a {
+                Action::Down => {
+                    let op = ctx.get_op(node).unwrap();
+                    match op {
+                        Op::Var(..) | Op::Input(..) | Op::Const(..) => {
+                            weights.insert(node, 1);
+                        }
+                        Op::Unary(..) | Op::Binary(..) => {
+                            todo.push((Action::Up, node));
+                            for c in op.iter_children() {
+                                todo.push((Action::Down, c));
+                            }
+                        }
+                    }
+                }
+                Action::Up => {
+                    let op = ctx.get_op(node).unwrap();
+                    let w: usize = op
+                        .iter_children()
+                        .map(|c| weights.get(&c).unwrap())
+                        .sum();
+                    weights.insert(node, w);
+                    if w <= max_weight {
+                        inline.insert(node);
+                    }
+                }
+            }
+        }
+        inline
+    }
+
     pub fn buildy(&mut self, root: Node) -> Result<(), Error> {
+        let inline = Self::pick_inline(self.ctx, root, 6);
+        println!("got {} inline nodes", inline.len());
+
         let mut todo = vec![(root, None)];
         while let Some((node, dnf)) = todo.pop() {
+            if inline.contains(&node) {
+                continue;
+            }
             let op = self.ctx.get_op(node).ok_or(Error::BadNode)?;
             if matches!(op, Op::Const(..)) {
                 continue;
@@ -86,7 +146,8 @@ impl<'a> Compiler<'a> {
             }
         }
 
-        let mut dnf_nodes: BTreeMap<_, Vec<Node>> = BTreeMap::new();
+        // Swap around, fron node -> DNF to DNF -> [Nodes]
+        let mut dnf_nodes: BTreeMap<_, BTreeSet<Node>> = BTreeMap::new();
         for (n, d) in &self.node_dnfs {
             println!(
                 "node {n:?} {:?} has {} DNFs",
@@ -96,17 +157,38 @@ impl<'a> Compiler<'a> {
             for d in d {
                 println!("    {d:?}");
             }
-            dnf_nodes.entry(d.clone()).or_default().push(*n);
+            dnf_nodes.entry(d.clone()).or_default().insert(*n);
+        }
+
+        // For each DNF, add all of the inlined nodes
+        for nodes in dnf_nodes.values_mut() {
+            let mut todo = BTreeSet::new();
+            for node in nodes.iter() {
+                let op = self.ctx.get_op(*node).unwrap();
+                todo.extend(op.iter_children().filter(|c| inline.contains(c)));
+            }
+            let mut todo: Vec<_> = todo.into_iter().collect();
+            while let Some(node) = todo.pop() {
+                if nodes.insert(node) {
+                    todo.extend(self.ctx.get_op(node).unwrap().iter_children());
+                }
+            }
         }
         println!("got {} nodes", self.node_dnfs.len());
         println!("got {} DNF -> node clusters", dnf_nodes.len());
+
         let mut hist: BTreeMap<_, usize> = BTreeMap::new();
         for d in dnf_nodes.values() {
             *hist.entry(d.len()).or_default() += 1;
         }
-        for (size, count) in hist {
+        for (size, count) in &hist {
             println!("{size} => {count}");
         }
+        println!("total functions: {}", hist.values().sum::<usize>());
+        println!(
+            "total instructions: {}",
+            hist.iter().map(|(a, b)| a * b).sum::<usize>()
+        );
         Ok(())
     }
 }
