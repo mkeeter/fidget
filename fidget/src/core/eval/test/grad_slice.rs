bebc31c59e4005081c7ecb4b9450e70ca4961d09
Matt Keeter <matt.j.keeter@gmail.com>
2024-03-16 16:32:19 -0400

    Add fuzzing for grad-slice evaluator

--- fidget/src/core/eval/test/grad_slice.rs
+++ fidget/src/core/eval/test/grad_slice.rs
@@ -17,6 +17,12 @@ macro_rules! grad_slice_unary {
     };
 }
 
+macro_rules! grad_slice_binary {
+    (Context::$i:ident, $t:expr) => {
+        Self::test_binary_reg_reg(Context::$i, $t, stringify!($i));
+    };
+}
+
 /// Helper struct to put constrains on our `Shape` object
 pub struct TestGradSlice<S>(std::marker::PhantomData<*const S>);
 
@@ -487,6 +493,118 @@ where
         }
     }
 
+    pub fn compare_grad_results(
+        i: usize,
+        j: usize,
+        lhs: &[f32],
+        rhs: &[f32],
+        out: &[Grad],
+        g: impl Fn(f64, f64) -> f64,
+        name: &'static str,
+    ) {
+        for ((a, b), &o) in lhs.iter().zip(rhs).zip(out.iter()) {
+            let v = g(*a as f64, *b as f64);
+            let err = (v as f32 - o.v).abs();
+            let err_frac = err / (v.abs() as f32).max(o.v.abs());
+            assert!(
+                (o.v == v as f32)
+                    || err < 1e-6
+                    || err_frac < 1e-6
+                    || (v.is_nan() && o.v.is_nan()),
+                "mismatch in '{name}' at {a}: {v} != {o} ({err})"
+            );
+
+            if v.is_nan() {
+                continue;
+            }
+
+            const EPSILON: f64 = 1e-8;
+            let a = *a as f64;
+            let b = *b as f64;
+            if i == j {
+                let grad = o.d(i);
+                if grad < 1e9 && !grad.is_infinite() {
+                    let d = g(a, b + EPSILON);
+                    let est_grad = (d - v) / EPSILON;
+                    let err = (est_grad as f32 - grad).abs();
+                    assert!(
+                        err < 1e-3,
+                        "gradient estimate mismatch in '{name}' at \
+                         ({a} + epsilon, {b}): \
+                         {est_grad} != {grad} ({err})"
+                    );
+                }
+            } else {
+                let grad = o.d(i);
+                if grad < 1e9 && !grad.is_infinite() {
+                    let d = g(a + EPSILON, b + EPSILON);
+                    let est_grad = (d - v) / EPSILON;
+                    let err = (est_grad as f32 - grad).abs();
+                    assert!(
+                        err < 1e-3,
+                        "gradient estimate mismatch in '{name}' at \
+                         ({a} + epsilon, {b}): \
+                         {est_grad} != {grad} ({err})"
+                    );
+                }
+                let grad = o.d(j);
+                if grad < 1e9 && !grad.is_infinite() {
+                    let d = g(a, b + EPSILON);
+                    let est_grad = (d - v) / EPSILON;
+                    let err = (est_grad as f32 - grad).abs();
+                    assert!(
+                        err < 1e-3,
+                        "gradient estimate mismatch in '{name}' at \
+                         ({a}, {b} + epsilon): \
+                         {est_grad} != {grad} ({err})"
+                    );
+                }
+            }
+        }
+    }
+
+    pub fn test_binary_reg_reg(
+        f: impl Fn(&mut Context, Node, Node) -> Result<Node, Error>,
+        g: impl Fn(f64, f64) -> f64,
+        name: &'static str,
+    ) {
+        let args = test_args();
+        let zero = vec![0.0; args.len()];
+
+        let mut ctx = Context::new();
+        let inputs = [ctx.x(), ctx.y(), ctx.z()];
+        for rot in 0..args.len() {
+            let mut rgsa = args.clone();
+            rgsa.rotate_left(rot);
+            for (i, &v) in inputs.iter().enumerate() {
+                for (j, &u) in inputs.iter().enumerate() {
+                    let node = f(&mut ctx, v, u).unwrap();
+
+                    let shape = S::new(&ctx, node).unwrap();
+                    let mut eval = S::new_grad_slice_eval();
+                    let tape = shape.ez_grad_slice_tape();
+
+                    let out = match (i, j) {
+                        (0, 0) => eval.eval(&tape, &args, &zero, &zero, &[]),
+                        (0, 1) => eval.eval(&tape, &args, &rgsa, &zero, &[]),
+                        (0, 2) => eval.eval(&tape, &args, &zero, &rgsa, &[]),
+                        (1, 0) => eval.eval(&tape, &rgsa, &args, &zero, &[]),
+                        (1, 1) => eval.eval(&tape, &zero, &args, &zero, &[]),
+                        (1, 2) => eval.eval(&tape, &zero, &args, &rgsa, &[]),
+                        (2, 0) => eval.eval(&tape, &rgsa, &zero, &args, &[]),
+                        (2, 1) => eval.eval(&tape, &zero, &rgsa, &args, &[]),
+                        (2, 2) => eval.eval(&tape, &zero, &zero, &args, &[]),
+                        _ => unreachable!(),
+                    }
+                    .unwrap();
+
+                    let rhs = if i == j { &args } else { &rgsa };
+                    Self::compare_grad_results(i, j, &args, rhs, out, &g, name);
+                }
+            }
+        }
+    }
+
     pub fn test_g_unary_ops() {
         grad_slice_unary!(Context::neg, |v| -v);
         grad_slice_unary!(Context::recip, |v| 1.0 / v);
@@ -501,6 +619,14 @@ where
         grad_slice_unary!(Context::ln, |v| v.ln());
         grad_slice_unary!(Context::square, |v| v * v);
         grad_slice_unary!(Context::sqrt, |v| v.sqrt());
+    }
+
+    pub fn test_g_binary_ops() {
+        grad_slice_binary!(Context::add, |a, b| a + b);
+        grad_slice_binary!(Context::sub, |a, b| a - b);
+        grad_slice_binary!(Context::mul, |a, b| a * b);
+        grad_slice_binary!(Context::div, |a, b| a / b);
+        grad_slice_binary!(Context::add, |a, b| a + b);
     }
 }
 
@@ -534,5 +660,6 @@ macro_rules! grad_slice_tests {
         $crate::grad_test!(test_g_var, $t);
         $crate::grad_test!(test_g_stress, $t);
         $crate::grad_test!(test_g_unary_ops, $t);
+        $crate::grad_test!(test_g_binary_ops, $t);
     };
 }
