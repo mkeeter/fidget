0679b73d2a04bf87ddaf272f4f132f78d935cafa
Matt Keeter <matt.j.keeter@gmail.com>
2024-03-21 08:18:31 -0400

    Fix AArch64 float slice <

--- fidget/src/jit/aarch64/float_slice.rs
+++ fidget/src/jit/aarch64/float_slice.rs
@@ -294,11 +294,30 @@ impl Assembler for FloatSliceAssembler {
 
     fn build_lt(&mut self, out_reg: u8, lhs_reg: u8, rhs_reg: u8) {
         dynasm!(self.0.ops
+            // Build a mask of valid positions (not NAN)
+            ; fcmeq v6.S4, V(reg(lhs_reg)).S4, V(reg(lhs_reg)).S4
+            ; fcmeq v7.S4, V(reg(rhs_reg)).S4, V(reg(rhs_reg)).S4
+            ; and v6.b16, v6.b16, v7.b16
+
+            // Invert to get a mask of NAN position
+            ; mvn v6.b16, v6.b16
+
             // Note the swap here, from LT -> GT
             ; fcmgt V(reg(out_reg)).S4, V(reg(rhs_reg)).S4, V(reg(lhs_reg)).S4
+            // At this point, out_reg is all 1s where we should put 1.0
+
+            // Copy over 1.0s, leaving 0s everywhere else (which is 0.0)
             ; fmov s7, 1.0
             ; dup v7.s4, v7.s[0]
             ; and V(reg(out_reg)).B16, V(reg(out_reg)).B16, v7.B16
+
+            // Build a NAN mask
+            ; mov w9, f32::NAN.to_bits().into()
+            ; dup v7.s4, w9
+            ; and v7.b16, v7.b16, v6.b16
+
+            // Apply NAN mask to NAN positions
+            ; orr V(reg(out_reg)).B16, V(reg(out_reg)).B16, v7.b16
         )
     }
 
