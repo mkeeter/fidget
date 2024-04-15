e1a7c2a9d253bc387d03498de9afa6f871cd7fc1
Matt Keeter <matt.j.keeter@gmail.com>
2024-04-15 08:00:38 -0400

    lol javascript

--- fidget-wasm-demo/src/lib.rs
+++ fidget-wasm-demo/src/lib.rs
@@ -9,5 +9,5 @@ pub struct JsTree(Tree);
 pub fn eval_script(s: &str) -> Result<JsTree, String> {
     let mut engine = fidget::rhai::Engine::new();
     let out = engine.eval(s);
-    out.map(JsTree).map_err(|e| format!("{e:?}"))
+    out.map(JsTree).map_err(|e| format!("{e}"))
 }
