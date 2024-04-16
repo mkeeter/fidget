591a9316b4c5a9bc0ac38e8da532fdc7f06bd27c
Matt Keeter <matt.j.keeter@gmail.com>
2024-04-15 22:41:45 -0400

    shenanigans

--- fidget-wasm-demo/index.js
+++ fidget-wasm-demo/index.js
@@ -1,17 +1,37 @@
-import {EditorView, keymap} from "@codemirror/view"
+import {EditorView, ViewPlugin, keymap} from "@codemirror/view"
 import {defaultKeymap} from "@codemirror/commands"
 
-let myView = new EditorView({
-  doc: "hello",
-  extensions: [keymap.of(defaultKeymap)],
-  parent: document.body
-})
-
 async function run() {
-    let fidget = await import('./pkg')
+    const fidget = await import('./pkg')
       .catch(console.error);
-    window.fidget = fidget;
+    var timeout = null;
 
+    function setScript(text) {
+        try {
+            let v = fidget.eval_script(text);
+            console.log(v);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    let myView = new EditorView({
+        doc: "hello",
+        extensions: [
+            keymap.of(defaultKeymap),
+            EditorView.updateListener.of(v => {
+                if (v.docChanged) {
+                    if (timeout) {
+                        clearTimeout(timeout);
+                        timeout = null;
+                    }
+                    let text = v.state.doc.toString();
+                    timeout = setTimeout(() => setScript(text), 500);
+                }
+            })
+        ],
+        parent: document.body
+    })
     console.log("booted");
 }
 run();
--- fidget-wasm-demo/webpack.config.js
+++ fidget-wasm-demo/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
