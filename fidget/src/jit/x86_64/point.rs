a1a853b804f9ba36273b10984c6fb2cf338d9ffa
Matt Keeter <matt.j.keeter@gmail.com>
2024-05-12 22:32:17 +0000

    lol x86

--- fidget/src/jit/x86_64/point.rs
+++ fidget/src/jit/x86_64/point.rs
@@ -180,22 +180,27 @@ impl Assembler for PointAssembler {
 
     // TODO optimize these three functions
     fn build_floor(&mut self, out_reg: u8, lhs_reg: u8) {
-        extern "sysv64" fn float_floor(f: f32) -> f32 {
-            f.floor()
-        }
-        self.call_fn_unary(out_reg, lhs_reg, float_floor);
+        dynasm!(self.0.ops
+            ; vroundss Rx(reg(out_reg)), Rx(reg(lhs_reg)), Rx(reg(lhs_reg)), 1
+        );
     }
     fn build_ceil(&mut self, out_reg: u8, lhs_reg: u8) {
-        extern "sysv64" fn float_ceil(f: f32) -> f32 {
-            f.ceil()
-        }
-        self.call_fn_unary(out_reg, lhs_reg, float_ceil);
+        dynasm!(self.0.ops
+            ; vroundss Rx(reg(out_reg)), Rx(reg(lhs_reg)), Rx(reg(lhs_reg)), 2
+        );
     }
     fn build_round(&mut self, out_reg: u8, lhs_reg: u8) {
-        extern "sysv64" fn float_round(f: f32) -> f32 {
-            f.round()
-        }
-        self.call_fn_unary(out_reg, lhs_reg, float_round);
+        // Shenanigans figured through Godbolt
+        dynasm!(self.0.ops
+            ; mov eax, 0x80000000u32 as i32
+            ; vmovd xmm1, eax
+            ; vandps  xmm1, xmm1, Rx(reg(lhs_reg))
+            ; mov eax, 0x3effffffu32 as i32
+            ; vmovd xmm2, eax
+            ; vorps  xmm1, xmm1, xmm2
+            ; vaddss Rx(reg(out_reg)), xmm1, Rx(reg(lhs_reg))
+            ; vroundss Rx(reg(out_reg)), Rx(reg(out_reg)), Rx(reg(out_reg)), 3
+        );
     }
 
     fn build_add(&mut self, out_reg: u8, lhs_reg: u8, rhs_reg: u8) {
