b834bcd681029ff2c1fc7f34452863c9b057719a
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-28 13:09:42 -0500

    Texture shenanigans

--- demos/web-editor/web/src/index.ts
+++ demos/web-editor/web/src/index.ts
@@ -382,12 +382,32 @@ class Scene {
 
     // XXX use mipmap levels instead?
     this.textures = [];
-    for (var depth=0; depth <= MAX_DEPTH; ++depth) {
+    for (var depth = 0; depth <= MAX_DEPTH; ++depth) {
       const texture = this.gl.createTexture();
       const size = Math.round(RENDER_SIZE / Math.pow(2, depth));
 
       // Bind an initial texture of the correct size
       this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+      this.gl.texParameteri(
+        this.gl.TEXTURE_2D,
+        this.gl.TEXTURE_WRAP_S,
+        this.gl.CLAMP_TO_EDGE,
+      );
+      this.gl.texParameteri(
+        this.gl.TEXTURE_2D,
+        this.gl.TEXTURE_WRAP_T,
+        this.gl.CLAMP_TO_EDGE,
+      );
+      this.gl.texParameteri(
+        this.gl.TEXTURE_2D,
+        this.gl.TEXTURE_MIN_FILTER,
+        this.gl.NEAREST,
+      );
+      this.gl.texParameteri(
+        this.gl.TEXTURE_2D,
+        this.gl.TEXTURE_MAG_FILTER,
+        this.gl.NEAREST,
+      );
       this.gl.texImage2D(
         this.gl.TEXTURE_2D,
         0,
