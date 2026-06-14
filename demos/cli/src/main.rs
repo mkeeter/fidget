068b444c2e5d3f618d1973003bfbca1b6a04efbb
Matt Keeter <matt.j.keeter@gmail.com>
2026-06-14 09:17:26 -0400

    Revert GeometryPixel back to storing a u32 depth

--- demos/cli/src/main.rs
+++ demos/cli/src/main.rs
@@ -347,7 +347,7 @@ fn postprocess3d(
             image
                 .into_iter()
                 .flat_map(|p| {
-                    if p.depth > 0.0 {
+                    if p.depth > 0 {
                         let c = p.to_color();
                         [c[0], c[1], c[2], 255]
                     } else {
@@ -368,7 +368,7 @@ fn postprocess3d(
                 .into_iter()
                 .zip(color)
                 .flat_map(|(p, c)| {
-                    if p.depth > 0.0 {
+                    if p.depth > 0 {
                         [c[0], c[1], c[2], 255]
                     } else {
                         [0, 0, 0, 0]
@@ -415,17 +415,12 @@ fn postprocess3d(
                 .collect()
         }
         RenderMode3D::Heightmap => {
-            let z_max = image
-                .iter()
-                .map(|p| ordered_float::OrderedFloat(p.depth))
-                .max()
-                .map(|p| p.0)
-                .unwrap_or(1.0);
+            let z_max = image.iter().map(|p| p.depth).max().unwrap_or(1);
             image
                 .into_iter()
                 .flat_map(|p| {
-                    if p.depth > 0.0 {
-                        let z = (p.depth * 255.0 / z_max).round() as u8;
+                    if p.depth > 0 {
+                        let z = (p.depth * 255 / z_max) as u8;
                         [z, z, z, 255]
                     } else {
                         [0, 0, 0, 0]
